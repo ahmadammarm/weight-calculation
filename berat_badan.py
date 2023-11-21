558804bfee46d62344209127e3a9ff3207750a94
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-21 21:55:58 +0700

    update formula

--- berat_badan.py
+++ berat_badan.py
@@ -1,16 +1,31 @@
+# Anggota Kelompok
+# 1. Abdullah Sholum (220535608686)
+# 2. Adil Zakaria (2205356080453)
+# 3. Ahmad Ammar Musyaffa (220535601431)
+# 4. Dafa Fadhilah Hilmi (220535610309)
+# 5. Emiriopriimo Nadyzar Baruna (220535604509)
+
+
 import cv2
 import mediapipe as mp
 
 def calculate_weight(shoulder_width):
     k = 2
-    estimated_weight = k * shoulder_width 
-    return round(estimated_weight)
+    scaling_factor = 0.4
+    estimated_weight = scaling_factor * k * shoulder_width
+    return round(estimated_weight) 
+
+def calculate_height(shoulder_width):
+    k = 5
+    scaling_factor = 0.4
+    estimated_height = scaling_factor * k * shoulder_width
+    return round(estimated_height)
 
 def main():
     mp_drawing = mp.solutions.drawing_utils
     mp_pose = mp.solutions.pose
 
-    cap = cv2.VideoCapture(0)  # Gunakan kamera
+    cap = cv2.VideoCapture(0)
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         while cap.isOpened():
             success, image = cap.read()
@@ -29,21 +44,24 @@ def main():
                 # Hitung lebar bahu berdasarkan landmark (dalam piksel)
                 shoulder_width_px = abs(right_shoulder.x - left_shoulder.x) * image.shape[1]
                 
+                # Tampilkan estimasi tinggi badan
+                estimated_height = calculate_height(shoulder_width_px)
+                cv2.putText(image, f"Estimated height: {estimated_height} cm", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                
                 # Hitung estimasi berat badan berdasarkan lebar bahu
                 estimated_weight = calculate_weight(shoulder_width_px)
+                cv2.putText(image, f"Estimated weight: {estimated_weight} kg", (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
                 
-                # Tampilkan hasil estimasi
-                cv2.putText(image, f"Estimated weight: {estimated_weight} kg", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 
                 # Tampilkan landmark pada tubuh
                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
             cv2.imshow('Weight Measurement', image)
-            if cv2.waitKey(5) & 0xFF == 27:
+            key = cv2.waitKey(5)
+            if key == 27 or cv2.getWindowProperty('Weight Measurement', cv2.WND_PROP_VISIBLE) < 1:
                 break
 
     cap.release()
     cv2.destroyAllWindows()
 
-if __name__ == "__main__":
-    main()
+main()
